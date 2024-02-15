0438890a8b50d74a206a9aa6b30c479b891a2a9f
Alpha Chen <alpha@kejadlen.dev>
2024-02-14 20:20:47 -0800

    auth

--- go_ji/__init__.py
+++ go_ji/__init__.py
@@ -1,8 +1,31 @@
-from flask import Flask
+import os
 
-from go_ji.db import db_session
+import sqlalchemy
+from flask import Flask, abort, request, session
+from sqlalchemy import select
+
+from go_ji.db import User, db_session
 
 app = Flask(__name__)
+app.secret_key = os.environ["FLASK_SECRET_KEY"]
+
+
+@app.before_request
+def require_user() -> None:
+    if "user_id" in session:
+        return
+
+    if login := request.headers.get("Tailscale-User-Login"):
+        try:
+            user = db_session.scalars(select(User).where(User.login == login)).one()
+        except sqlalchemy.exc.NoResultFound:
+            user = User(login=login)
+            db_session.add(user)
+            db_session.commit()
+        session["user_id"] = user.id
+        return
+
+    abort(403)
 
 
 @app.teardown_appcontext
