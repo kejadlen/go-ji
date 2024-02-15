7c01b8f64170ab830a736175bd3ef37fde07cad6
Alpha Chen <alpha@kejadlen.dev>
2024-02-14 19:56:26 -0800

    put all db things into go_ji.db

--- go_ji/db.py
+++ go_ji/db.py
@@ -1,6 +1,17 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
+from datetime import datetime
+from typing import List
+
+from sqlalchemy import ForeignKey, Text, create_engine
+from sqlalchemy.orm import (
+    Mapped,
+    declarative_base,
+    mapped_column,
+    relationship,
+    scoped_session,
+    sessionmaker,
+)
 from sqlalchemy.schema import MetaData
+from sqlalchemy.sql import func
 
 engine = create_engine("sqlite:///go-ji.db")
 # engine = create_engine("sqlite:///:memory:")
@@ -20,7 +31,33 @@ convention = {
 }
 Base.metadata = MetaData(naming_convention=convention)
 
-def init_db():
-    import go_ji.models  # noqa: F401
 
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    login: Mapped[str] = mapped_column(Text(), nullable=False, unique=True)
+
+    created_at: Mapped[datetime] = mapped_column(nullable=False, default=func.now())
+
+
+class Short(Base):
+    __tablename__ = "shorts"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    slug: Mapped[str] = mapped_column(Text(), nullable=False, unique=True)
+    longs: Mapped[List["Long"]] = relationship(back_populates="short")
+
+    created_at: Mapped[datetime] = mapped_column(nullable=False, default=func.now())
+
+
+class Long(Base):
+    __tablename__ = "longs"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    url: Mapped[str] = mapped_column(Text(), nullable=False)
+    short_id: Mapped[int] = mapped_column(ForeignKey("shorts.id"), nullable=False)
+    short: Mapped["Short"] = relationship(back_populates="longs")
+
+    created_at: Mapped[datetime] = mapped_column(nullable=False, default=func.now())
+
+
+def init_db():
     Base.metadata.create_all(bind=engine)
