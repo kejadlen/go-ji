58350dd7b3cc79f6f8209346bc5343f05ef55f0a
Alpha Chen <alpha@kejadlen.dev>
2024-02-15 12:14:04 -0800

    log out of tailscale on being killed

--- start.sh
+++ start.sh
@@ -3,5 +3,9 @@
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=go
 
+trap '/app/tailscale logout' INT
+
 /app/tailscale serve --bg --http=80 8000
-gunicorn -b 0.0.0.0 go_ji:app
+gunicorn --daemon --bind 0.0.0.0 go_ji:app
+
+wait "$!"
