119802e3cc41b951e39538f3f766883a20d11078
Alpha Chen <alpha@kejadlen.dev>
2024-02-15 11:27:46 -0800

    deploy to fly

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=go
 
-gunicorn go_ji:app
+/app/tailscale serve --bg --http=80 8000
+gunicorn -b 0.0.0.0 go_ji:app
