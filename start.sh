bb2001b7b17a2b04769107a8fb16b6a98a8bfdfb
Alpha Chen <alpha@kejadlen.dev>
2024-02-15 10:34:25 -0800

    go

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=go-ji
+/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=go
 
 gunicorn go_ji:app
