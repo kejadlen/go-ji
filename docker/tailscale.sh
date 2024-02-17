6d242e9de4b7573f8f8a39de221da53da696e03c
Alpha Chen <alpha@kejadlen.dev>
2024-02-16 16:26:25 -0800

    reset tailscale serve on launch

--- docker/tailscale.sh
+++ docker/tailscale.sh
@@ -3,4 +3,5 @@
 mkdir -p /data/tailscale
 /app/tailscaled --state=/data/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME}
+/app/tailscale serve reset
 /app/tailscale serve --http=80 8000
